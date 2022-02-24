2df1f3306b6319cc5282bf7b2fef452f46530bbd
bxbi <93929459+bxbi-zxc@users.noreply.github.com>
2022-02-24 13:55:24 +0600

    Add files via upload

---
+++ 2/main.js
@@ -0,0 +1,4 @@
+let div = document.createElement('div');
+let textNode = document.createTextNode('выходит колобок из душа и говорит, блин голову забыл помыть');
+document.body.append(div);
+div.append(textNode);
